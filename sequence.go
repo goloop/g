2cc7ea056726c9a4f8ce87d0ef13b46456a9fb27
goloop <goloop.one@gmail.com>
2023-06-05 00:53:27 +0300

    Fix doc

--- sequence.go
+++ sequence.go
@@ -595,16 +595,16 @@ func In[T Verifiable](v T, list ...T) bool {
 // Example usage:
 //
 //	result := Range(5)
-//	// result: [0, 1, 2, 3, 4]
+//	// Output: [0, 1, 2, 3, 4]
 //
 //	result := Range(3, 7)
-//	// result: [3, 4, 5, 6]
+//	// Output: [3, 4, 5, 6]
 //
 //	result := Range(1, 10, 2)
-//	// result: [1, 3, 5, 7, 9]
+//	// Output: [1, 3, 5, 7, 9]
 //
 //	result := Range(10, 0, -1)
-//	// result: [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+//	// Output: [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
 func Range(a int, opt ...int) []int {
 	var n, m, s int = 0, a, 1
 
